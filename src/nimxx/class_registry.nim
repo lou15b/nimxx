9ab8f043846f3f7eaeb3f9b00195e461ce1fc906
Louis Berube <louis.p.berube@gmail.com>
2024-03-17 10:26:31 -0400

    Fixed class_registry.nim
    - It now works for "object" types as well as "ref object" types
    - Generates a compile error if the type being registered is not
      derived from either RootObj or RootRef

--- src/nimxx/class_registry.nim
+++ src/nimxx/class_registry.nim
@@ -2,6 +2,8 @@ import std / [ tables, macros ]
 import pkg/variant
 import std/typetraits except getTypeid, Typeid # see https://github.com/nim-lang/Nim/pull/13305
 
+export variant
+
 proc skipPtrRef(n: NimNode): NimNode =
   let ty = getImpl(n)
   result = n
@@ -20,6 +22,9 @@ proc `*`(s: string, i: int): string {.compileTime, used.} =
 
 proc superTypeAux(t: NimNode, indent: int): NimNode =
   doAssert(indent < 10, "Recursion too deep")
+
+  # if indent == 0:
+  #   echo "\n superTypeAux input AST = ", treerepr(t)
 
   template superTypeAux(t: NimNode): NimNode = superTypeAux(t, indent + 1)
   proc log(args: varargs[string, `$`]) =
@@ -49,8 +54,11 @@ proc superTypeAux(t: NimNode, indent: int): NimNode =
     doAssert(false, "Unknown node")
 
   log "result ", repr(result)
+  # if indent == 0:
+  #   echo " superTypeAux final AST result = ", treerepr(result)
 
-macro superType*(t: typed): untyped = superTypeAux(t, 0)
+macro superType*(t: typed): untyped =
+  result = superTypeAux(t, 0)
 
 method className*(o: RootRef): string {.base, gcsafe.} = discard
 method classTypeId*(o: RootRef): TypeId {.base, gcsafe.} = getTypeId(RootRef)
@@ -66,15 +74,49 @@ type ClassInfo = tuple
 #   "{.gcsafe.}:" block.
 #   See proc newObjectOfClass and the registeredX iterators
 var classFactory: Table[string, ClassInfo]
-var superTypeRelations: Table[TypeId, TypeId]
+var superTypeRelations*: Table[TypeId, TypeId]
 
-{.push, stackTrace: off.}
+proc addSuperTypeRelation*(typeId, superTypeId: TypeId) =
+  discard superTypeRelations.hasKeyOrPut(typeId, superTypeId)
 
-proc registerTypeRelation(a: typedesc) =
-  type ParentType = superType(a)
-  if not superTypeRelations.hasKeyOrPut(getTypeId(a), getTypeId(ParentType)):
-    when (RootRef isnot ParentType) and (RootObj isnot ParentType):
-      registerTypeRelation(ParentType)
+proc isRootSym(typeSym: NimNode): bool =
+  ## Determines whether the symbol is RootObj or RootRef
+  typeSym.expectKind(nnkSym)
+  let symName = typeSym.strVal
+  result = symName == "RootObj" or symName == "RootRef"
+
+macro registerSuperTypeRelations(a: untyped): untyped =
+  ## Generates an AST equivalent to a sequence of statements that adds
+  ## supertype entries to the superTypeRelations table, for all of the supertypes
+  ## of the argument a to the root type (RootObj or RootRef)
+  a.expectKind(nnkSym)
+  if a.isRootSym:
+    discard
+
+  result = newNimNode(nnkStmtList)
+  var typeSym = a
+  while not typeSym.isRootSym:
+    let superTypeSym = superTypeAux(typeSym, 0)
+
+    # Set 'addSuperTypeRelationCall' to an AST equivalent to the call:
+    #   'addSuperTypeRelation(getTypeId(type), getTypeId(supertype))'
+    let addSuperTypeRelationCall = newNimNode(nnkCall).add(
+      ident("addSuperTypeRelation"),
+      newNimNode(nnkCall).add(
+        ident("getTypeId"),
+        ident($typeSym)
+      ),
+      newNimNode(nnkCall).add(
+        ident("getTypeId"),
+        ident($superTypeSym)
+      )
+    )
+    result.add(addSuperTypeRelationCall)
+
+    # Loop back to generate the next call - for the supertype
+    typeSym = superTypeSym
+    
+  # echo "=== registerSuperTypeRelations generated the calls:", repr(result), "\n"
 
 proc isTypeOf(tself, tsuper: TypeId): bool =
   var t = tself
@@ -85,25 +127,36 @@ proc isTypeOf(tself, tsuper: TypeId): bool =
 proc isSubtypeOf(tself, tsuper: TypeId): bool =
   tself != tsuper and isTypeOf(tself, tsuper)
 
-{.pop.}
-
-template registerClass*(a: typedesc, creator: proc(): RootRef) =
+template registerClassImpl(a: typedesc, creator: proc(): RootRef) =
   const TName = typetraits.name(a)
+  # static: echo "registerClass: ", TName
   const tid = getTypeId(a)
   method className*(o: a): string = TName
   method classTypeId*(o: a): TypeId = tid
-  registerTypeRelation(a)
+  registerSuperTypeRelations(a)
   var info: ClassInfo
   info.creatorProc = creator
   info.typ = tid
   classFactory[TName] = info
 
-template registerClass*(a: typedesc) =
+template registerClassImpl(a: typedesc) =
   let c = proc(): RootRef =
     var res: a
     res.new()
     return res
-  registerClass(a, c)
+  registerClassImpl(a, c)
+
+template registerClass*(a: typedesc, creator: proc(): RootRef) =
+  when (a isnot RootObj) and (a isnot RootRef):
+    {.error: "registerClass is restricted to objects derived from RootObj or RootRef".}
+  elif (RootObj isnot a) and (RootRef isnot a):
+    registerClassImpl(a, creator)
+
+template registerClass*(a: typedesc) =
+  when (a isnot RootObj) and (a isnot RootRef):
+    {.error: "registerClass is restricted to objects derived from RootObj or RootRef".}
+  elif (RootObj isnot a) and (RootRef isnot a):
+    registerClassImpl(a)
 
 template isClassRegistered*(name: string): bool = name in classFactory
 
@@ -132,6 +185,20 @@ iterator registeredSubclassesOfType*(T: typedesc): string =
         yield k
 
 when isMainModule:
+  # # Should get a compile error when attempting to register a type that is
+  # # not derived from RootObj or RootRef
+  # type X = object
+  #   name: string = "Xname"
+  # registerClass(X)
+
+  template sameType(t1, t2: typedesc): bool =
+    t1 is t2 and t2 is t1
+
+  proc isSubtypeOf(tself, tsuper: string): bool =
+    isSubtypeOf(classFactory[tself].typ, classFactory[tsuper].typ)
+
+  # =========================
+
   type A = ref object of RootRef
   type B = ref object of A
   type C = ref object of B
@@ -140,12 +207,10 @@ when isMainModule:
   echo "typeId RootObj: ", getTypeId(RootObj)
   echo "typeId A: ", getTypeId(A)
   echo "typeId B: ", getTypeId(B)
+  echo "typeId C: ", getTypeId(C)
 
   echo "typeId superType(A): ", getTypeId(superType(A))
   echo "typeId superType(B): ", getTypeId(superType(B))
-
-  template sameType(t1, t2: typedesc): bool =
-    t1 is t2 and t2 is t1
 
   assert sameType(superType(A), RootRef)
   assert sameType(superType(B), A)
@@ -158,14 +223,14 @@ when isMainModule:
   doAssert(superTypeRelations[getTypeId(A)] == getTypeId(RootRef))
   doAssert(superTypeRelations[getTypeId(B)] == getTypeId(A))
 
-  proc isSubtypeOf(tself, tsuper: string): bool =
-    isSubtypeOf(classFactory[tself].typ, classFactory[tsuper].typ)
-
   doAssert("B".isSubtypeOf("A"))
   doAssert(not "A".isSubtypeOf("B"))
 
   echo "Supertype relations: ", superTypeRelations
 
+  echo "Subclasses of RootObj:"
+  for t in registeredClassesOfType(RootObj):
+    echo t
   echo "Subclasses of RootRef:"
   for t in registeredClassesOfType(RootRef):
     echo t
@@ -188,9 +253,61 @@ when isMainModule:
   doAssert(b.classTypeId() == getTypeId(B))
   doAssert(c.classTypeId() == getTypeId(C))
 
-  proc getSupertypeTypeId(a: typedesc): TypeId =
-    type ParentType = superType(a)
-    const id = getTypeId(ParentType)
-    return id
+  # =========================
+  superTypeRelations.clear()
+  echo "========================="
 
-  doAssert(getSupertypeTypeId(A) == getTypeId(RootRef))
+  type AA = object of RootObj
+  type AAR = ref AA
+  type BBR = ref object of AAR
+  type CCR = ref object of BBR
+
+  echo "typeId AA: ", getTypeId(AA)
+  echo "typeId AAR: ", getTypeId(AAR)
+  echo "typeId BBR: ", getTypeId(BBR)
+  echo "typeId CCR: ", getTypeId(CCR)
+
+  echo "typeId superType(A): ", getTypeId(superType(A))
+  echo "typeId superType(B): ", getTypeId(superType(B))
+
+  assert sameType(superType(AA), RootObj)
+  assert sameType(superType(AAR), RootObj)
+  assert sameType(superType(BBR), AAR)
+  assert sameType(superType(CCR), BBR)
+
+  registerClass(AAR)
+  registerClass(BBR)
+  registerClass(CCR)
+
+  doAssert(superTypeRelations[getTypeId(AAR)] == getTypeId(RootObj))
+  doAssert(superTypeRelations[getTypeId(BBR)] == getTypeId(AAR))
+
+  doAssert("BBR".isSubtypeOf("AAR"))
+  doAssert(not "AAR".isSubtypeOf("BBR"))
+
+  echo "Supertype relations: ", superTypeRelations
+
+  echo "Subclasses of RootObj:"
+  for t in registeredClassesOfType(RootObj):
+    echo t
+  echo "Subclasses of RootRef:"
+  for t in registeredClassesOfType(RootRef):
+    echo t
+  echo "Subclasses of AAR:"
+  for t in registeredClassesOfType(AAR):
+    echo t
+  echo "Subclasses of BBR:"
+  for t in registeredClassesOfType(BBR):
+    echo t
+
+  let aa = newObjectOfClass("AAR")
+  let bb = newObjectOfClass("BBR")
+  let cc = newObjectOfClass("CCR")
+
+  doAssert(aa.className() == "AAR")
+  doAssert(bb.className() == "BBR")
+  doAssert(cc.className() == "CCR")
+
+  doAssert(aa.classTypeId() == getTypeId(AAR))
+  doAssert(bb.classTypeId() == getTypeId(BBR))
+  doAssert(cc.classTypeId() == getTypeId(CCR))
